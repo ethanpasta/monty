b4f94aa6372d05d796f7a661d0fb202646b99bba
ethanpasta <ethan.mayer9@gmail.com>
2019-05-14 19:17:50 +0000

    Initial main file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * main - entry point
+ * @ac: number of arguments
+ * @av: array of arguments
+ *
+ * Return: exit status
+ */
+int main(int ac, __attribute__((unused))char **av)
+{
+	if (ac != 2)
+		file_err(NULL);
+	return 1;
+}
