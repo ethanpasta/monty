a32fdc315488617a77a12c30499182d412fd68b8
ethanpasta <ethan.mayer9@gmail.com>
2019-05-15 20:51:23 +0000

    Implementation of pop

--- opcode_f.c
+++ opcode_f.c
@@ -6,32 +6,44 @@ void push_o(stack_t **stack, unsigned int line_number)
 	char *num;
 
 	(void)line_number;
-	num = strtok(opcode_g, " ");
+	num = strtok(main_s->opcode_g, " ");
 	num = strtok(NULL, " ");
 	if (!num || !isdigit(num[0]))
-		push_no_arg(line_number);
+	{
+		free_stuff(*stack);
+		dprintf(2, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	n = atoi(num);
-	push(stack, n);
+	add_dnodeint(stack, n);
 }
 
 void pall_o(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
-
 	(void)line_number;
-	print_stack(*stack);
+	print_dlistint(*stack);
 }
 
 void pint_o(stack_t **stack, unsigned int line_number)
 {
-        (void)stack;
-        (void)line_number;
+	if (!*stack)
+	{
+		dprintf(2, "L%d can't pint, stack empty\n", line_number);
+		free_stuff(*stack);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
 
 void pop_o(stack_t **stack, unsigned int line_number)
 {
-        (void)stack;
-        (void)line_number;
+	if (!*stack)
+	{
+		dprintf(2, "L%d: can't pop an empty stack\n", line_number);
+		free_stuff(*stack);
+		exit(EXIT_FAILURE);
+	}
+	delete_dnodeint_head(stack);
 }
 
 void swap_o(stack_t **stack, unsigned int line_number)
