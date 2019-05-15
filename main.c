a7ea65c8a4659075dd9f1d662dc67f468347271c
ethanpasta <ethan.mayer9@gmail.com>
2019-05-15 02:51:41 +0000

    Change main to use helper function

--- main.c
+++ main.c
@@ -7,9 +7,27 @@
  *
  * Return: exit status
  */
-int main(int ac, __attribute__((unused))char **av)
+int main(int ac, char **av)
 {
+	instruction_t coms[] = {
+		{"push", push_o},
+		{"pall", pall_o},
+		{"pint", pint_o},
+		{"pop", pop_o},
+		{"swap", swap_o},
+		{"add", add_o},
+		{"nop", nop_o},
+		{NULL, NULL}
+	};
+	stack_t *stack = NULL;
+	FILE *fp;
+
 	if (ac != 2)
 		file_err(NULL);
+	fp = fopen(av[1], "r");
+	if (!fp)
+		file_err(av[1]);
+	main_loop(&stack, fp, coms);
+	fclose(fp);
 	return 1;
 }
