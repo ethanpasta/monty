5bcf48a4b147acba3b842dc139d126250399711c
ethanpasta <ethan.mayer9@gmail.com>
2019-05-15 20:50:27 +0000

    Remove unnecessary error functions

--- error_f.c
+++ error_f.c
@@ -8,23 +8,22 @@ void file_err(char *file)
 		exit(EXIT_FAILURE);
 	}
 	printf("Error: Can't open file %s\n", file);
+	free(main_s);
 	exit(EXIT_FAILURE);
 }
 
 void malloc_fail()
 {
 	printf("Error: malloc failed\n");
-	exit(EXIT_FAILURE);
-}
 
-void push_no_arg(unsigned int line_n)
-{
-	printf("L%u: usage: push integer\n", line_n);
 	exit(EXIT_FAILURE);
 }
 
 void invalid_code(unsigned int line_n, char *opcode)
 {
-	printf("L%u: unknown instruction %s\n", line_n, opcode);
+	printf("L%u: unknown instruction %s", line_n, opcode);
+	free(main_s->opcode_g);
+	fclose(main_s->fp);
+	free(main_s);
 	exit(EXIT_FAILURE);
 }
