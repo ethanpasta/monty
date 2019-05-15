7c62992b0bc853f68f2dccbdfd23aa72c22effa3
ethanpasta <ethan.mayer9@gmail.com>
2019-05-15 20:50:34 +0000

    Add free function

--- help_main.c
+++ help_main.c
@@ -1,37 +1,51 @@
 #include "monty.h"
 
-char *opcode_g = NULL;
-
-void main_loop(stack_t **stack, FILE *fp, instruction_t coms[])
+void main_loop(stack_t **stack, instruction_t coms[])
 {
-        char *buffer = malloc(sizeof(char) * 32);
-	size_t buff_s = 32;
+        char *buffer = NULL;
+	size_t buff_s = 0;
 	int i, line_n = 1;
 
-	while (getline(&buffer, &buff_s, fp) != -1)
+	while (getline(&buffer, &buff_s, main_s->fp) != -1)
 	{
 		i = 0;
 		while (buffer[i] == ' ')
 			i++;
 		if (buffer[i] == '\n')
-			continue;
-		opcode_g = strdup(buffer + i);
-		free(buffer);
-		buffer = NULL;
-		for (i = 0; opcode_g && coms[i].f; i++)
 		{
-			if (strncmp(opcode_g, coms[i].opcode, 4) == 0)
+			line_n++;
+			continue;
+		}
+		main_s->opcode_g = buffer + i;
+		for (i = 0; main_s->opcode_g && coms[i].f; i++)
+		{
+			if (strncmp(main_s->opcode_g, coms[i].opcode, strlen(coms[i].opcode)) == 0)
 			{
 				coms[i].f(stack, line_n);
 				break;
 			}
 		}
 		if (coms[i].f == NULL)
-			invalid_code(line_n, opcode_g);
-		free(opcode_g);
-		opcode_g = NULL;
+		{
+			free(buffer);
+			free_dlistint(*stack);
+			invalid_code(line_n, main_s->opcode_g);
+		}
 		line_n++;
+		free(buffer);
+		buffer = NULL;
 	}
+	free_dlistint(*stack);
 	free(buffer);
-	free_stack(*stack);
+	fclose(main_s->fp);
+	free(main_s);
+}
+
+void free_stuff(stack_t *stack)
+{
+	fclose(main_s->fp);
+	free_dlistint(stack);
+	if (main_s->opcode_g)
+		free(main_s->opcode_g);
+	free(main_s);
 }
