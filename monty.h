576da56132ca46ee36acf84bd340032673615746
ethanpasta <ethan.mayer9@gmail.com>
2019-05-16 18:23:25 +0000

    Add new function prototypes

--- monty.h
+++ monty.h
@@ -63,6 +63,8 @@ void pop_o(stack_t **, unsigned int);
 void swap_o(stack_t **, unsigned int);
 void add_o(stack_t **, unsigned int);
 void nop_o(stack_t **, unsigned int);
+void sub_o(stack_t **, unsigned int);
+
 void main_loop(instruction_t *);
 void free_stuff(void);
 int execute_command(char *, int, instruction_t []);
