0a2925cb73cc335329be5a8c8e71080327fe832e
ethanpasta <ethan.mayer9@gmail.com>
2019-05-16 19:24:23 +0000

    Recopy error message

--- opcode_f2.c
+++ opcode_f2.c
@@ -58,6 +58,12 @@ void div_o(stack_t **stack, unsigned int line_number)
 		free_stuff();
 		exit(EXIT_FAILURE);
 	}
+	if ((*stack)->n == 0)
+	{
+		dprintf(2, "L%d: division by zero\n", line_number);
+		free_stuff();
+		exit(EXIT_FAILURE);
+	}
 	(*stack)->next->n = (*stack)->n / (*stack)->next->n;
 	delete_dnodeint_head(stack);
 }
